e0b9192c0ef227b8c235f38789ae65df33e6ab48
Gurditt Singh Khurana <gurdittsingh1508@gmail.com>
2022-06-23 15:51:55 +0530

    practicing

--- 6. 30th April 2022/003Arrays_BubbleSort.cpp
+++ 6. 30th April 2022/003Arrays_BubbleSort.cpp
@@ -4,24 +4,19 @@ using namespace std;
 
 int main() {
 
-	int A[] = {10, 20, 30, 40, 50};
+	int A[] = {50, 40, 30, 20, 10};
 	int n = sizeof(A)/sizeof(int);
 	int count = 0;
 	for(int i=1; i<n; i++) {
 		// in the ith pass place the largest element in the
 		// unsorted part of the array to its correct position.
-		bool flag = false; //assume array is sorted
 		for(int j=0; j<n-i; j++) {
-			count++;
 			if(A[j] > A[j+1]) {
 				swap(A[j], A[j+1]);
 			}
 		}
-		if(!flag){
-			break;
-		}
+
 	}
-	cout << count << endl;
  
 	for(int i=0; i<n; i++) {
 		cout << A[i] << " ";
